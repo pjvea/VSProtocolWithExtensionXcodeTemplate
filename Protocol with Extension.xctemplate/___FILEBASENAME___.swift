0af64552d52c6c2d9c506bd352ee32bf75f58bac
pjvea <pjvea13@gmail.com>
2017-01-23 08:47:40 -0700

    first commit

---
+++ Protocol with Extension.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,19 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+import Foundation
+
+protocol ___FILEBASENAMEASIDENTIFIER___ {
+    func ___VARIABLE_Method___()
+}
+
+extension ___FILEBASENAMEASIDENTIFIER___ {
+    func ___VARIABLE_Method___() {
+        
+    }
+}
